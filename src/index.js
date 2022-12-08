f0e850973ecbad197fc42e8ae2766e4a8a9158e3
Chase Lukasiewicz <chaseluka@gmail.com>
2022-12-08 16:06:27 -0700

    style

--- src/index.js
+++ src/index.js
@@ -1,35 +1,35 @@
-import { getWeather } from './api'
+import { getWeather } from './api';
 import { tempConversion } from './convert';
-
+import './style.css';
 
 const form = document.getElementById('location');
-const search = document.getElementById('search')
-const fahrenheitBtn = document.querySelector('.f')
+const search = document.getElementById('search');
+const fahrenheitBtn = document.querySelector('.f');
 const celsiusBtn = document.querySelector('.c');
 
-getWeather('New York')
+getWeather('New York');
 
 // Allow input of a location and return the weather
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  getWeather(`${search.value}`)
+  getWeather(`${search.value}`);
 });
 
 // Remove error sign on focus of input
 search.addEventListener('focus', () => {
   const errorMessage = document.querySelector('.error');
   errorMessage.style.display = 'none';
-})
+});
 
 // Recall the weather info based off users selection of fahr or cels
 fahrenheitBtn.addEventListener('click', () => {
   console.log(tempConversion.values().fahr);
-  if (tempConversion.values().cels === true){
+  if (tempConversion.values().cels === true) {
     tempConversion.switchMetric();
     console.log('hello');
     getWeather(`${search.value}`);
   }
-})
+});
 
 celsiusBtn.addEventListener('click', () => {
   console.log(tempConversion.values().fahr);
